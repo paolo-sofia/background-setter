cff30742f4c5d72db72c4397f4b9587152803679
Paolo <paolosofia@tutanota.com>
2023-06-11 15:16:17 +0200

    feat: implementation of wayland

--- background_setter/window_protocol/wayland.py
+++ background_setter/window_protocol/wayland.py
@@ -2,21 +2,21 @@ import pathlib
 from typing import List
 
 from background_setter.screen.screen import Screen
-from background_setter.window_protocol.window_protocol import WindowProtocol
+from background_setter.window_protocol.x11 import X11
 
 
-class Wayland(WindowProtocol):
+class Wayland(X11):
     pass
 
-    @staticmethod
-    def get_screens() -> List[Screen]:
-        pass
-
-    def update_background_image(self, image_path: pathlib.Path) -> None:
-        pass
-
-    def update_gnome_background(self, image_path: pathlib.Path) -> None:
-        pass
-
-    def update_kde_background(self, image_path: pathlib.Path) -> None:
-        pass
+    # @staticmethod
+    # def get_screens() -> List[Screen]:
+    #     pass
+    #
+    # def update_background_image(self, image_path: pathlib.Path) -> None:
+    #     pass
+    #
+    # def update_gnome_background(self, image_path: pathlib.Path) -> None:
+    #     pass
+    #
+    # def update_kde_background(self, image_path: pathlib.Path) -> None:
+    #     pass
